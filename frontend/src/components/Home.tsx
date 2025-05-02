c989d0f1544da60400c464c2970eefadfafc63aa
cisse17 <119404406+cisse17@users.noreply.github.com>
2025-05-02 16:05:29 +0200

    travailler sur la page Home

--- frontend/src/components/Home.tsx
+++ frontend/src/components/Home.tsx
@@ -1,6 +1,8 @@
 import { useEffect, useState } from 'react';
 import axios from 'axios';
-
+import bassirou from "../assets/bassirou.jpg"
+import { Mail, LucideGithub, Linkedin, Divide } from 'lucide-react';
+import {Link} from "react-router-dom"
 interface Projet {
   id: number;
   titre: string;
@@ -25,15 +27,62 @@ function Home() {
   // }, []);
 
   return (
-   <div className="bg-gray-50 ">
-    hello <br></br>
-    sjfbkzejlo <br></br>
-    fzekjbhlo <br></br>
-    zfzhjblo <br></br>
-    qdzgelo <br></br>
+    <div>
+    <div className="bg-gray-50 flex flex-col-reverse md:flex-row m-auto">
+    
+        <section className="mt-0 md:mt-15 m-auto flex flex-col items-center space-y-4 ">
+          <div className="flex items-center">
+            <h1 className="text-xl font-bold">Bassirou Mbacké CISSE</h1>
+          </div>
+          <h2 className="text-xl text-primary text-center">
+            Développeur Full-Stack · DevOps · LLM Enthusiast
+          </h2>
+    
+          <div className="w-full max-w-xl bg-base-200 shadow-lg">
+            <div className="card-body text-center text-lg font-serif">
+              <p>
+                Passionné par la technologie, je conçois et déploie des solutions robustes,
+                intelligentes et scalables. Mon expertise couvre le développement web,
+                les pipelines DevOps et l'intégration de modèles LLM pour créer des
+                produits modernes et performants.
+              </p>
+            </div>
+          </div>
+    
+          <div className="flex space-x-6 text-3xl mt-5">
+           <Link to={""} > <Mail /> </Link> 
+           <Link to={""} > <LucideGithub/> </Link> 
+           <Link to={""} > <Linkedin/> </Link> 
+            
+          </div>
+    
+          <Link to="mailto:bassiroucisse1711@gmail.com" className="btn btn-primary btn-wide text-lg mt-4">
+            Me contacter
+          </Link>
+        </section>
 
+        <section className='m-auto mt-15 mb-5 flex justify-center md:ml-1'>
+          <img src={bassirou} alt='' className='w-96 h-96  object-cover border-5 shadow-xl '
+          style= {{
+            borderRadius : "47% 53% 39% 61% / 38% 34% 66% 62% ", 
+            borderColor: "1727D7" 
+          }}
+          />
+        </section>
+        
+      {/* "truncate text-sm text-gray-500  */}
+    </div>
 
-   </div>
+    <div>
+      qfgbzzfeh <br></br>
+      qfgbzzfeh <br></br>
+      qfgbzzfeh <br></br>
+      qfgbzzfeh <br></br>
+      qfgbzzfeh <br></br>
+      qfgbzzfeh <br></br>
+    </div>
+    </div>
+    
   );
 }
 
