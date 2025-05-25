8f5c4071d978e92de2703cc2a6bb6995b9462bf0
cisse17 <119404406+cisse17@users.noreply.github.com>
2025-05-25 20:24:21 +0200

    modification de ma base de données en Postgresql

--- backend/site_pro/settings.py
+++ backend/site_pro/settings.py
@@ -79,23 +79,13 @@ WSGI_APPLICATION = 'site_pro.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
-
 # DATABASES = {
 #     'default': {
-#         'ENGINE': 'django.db.backends.postgresql',
-#         'NAME': 'portfolio_db',
-#         'USER': 'portfolio_user',
-#         'PASSWORD': 'motdepassefort',
-#         'HOST': 'localhost',
-#         'PORT': '5432',
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
 #     }
 # }
+
 
 
 DATABASES = {
@@ -112,7 +102,7 @@ DATABASES = {
 
 
 CORS_ALLOWED_ORIGINS = [
-    'http://localhost:5173',
+    'http://localhost:3000',
 ]
 
 # pour gerer la pagination 
